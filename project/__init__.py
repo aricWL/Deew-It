81d9a54da7622d6d6fae6210bc7c75cc4bf42a4d
aricWL <aricliesenfelt@gmail.com>
2017-02-25 11:35:52 -0800

    updating

--- project/__init__.py
+++ project/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 if os.environ.get('ENV') == 'production':
     debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 else:
     debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/deewit'
